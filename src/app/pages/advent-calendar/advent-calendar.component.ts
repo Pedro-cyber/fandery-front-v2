938870bb45ec10cefff9a65d04767f8a9c657936
Pedro García <pedrogarcia@Pedros-MacBook-Pro.local>
2025-12-09 08:47:02 +0100

    dia 9

--- src/app/pages/advent-calendar/advent-calendar.component.ts
+++ src/app/pages/advent-calendar/advent-calendar.component.ts
@@ -47,7 +47,8 @@ export class AdventCalendarComponent implements OnInit {
       5: { id: "76327", price: 39.99, original: 59.99 },
       6: { id: "60445", price: 50.00, original: 99.99 },
       7: { id: "76280", price: 22.35, original: 39.99 },
-      8: { id: "40743", price: 28.04, original: 39.99 }
+      8: { id: "40743", price: 28.04, original: 39.99 },
+      9: { id: "76304", price: 68.49, original: 99.99 }
     };
 
     for (const day of this.days) {
